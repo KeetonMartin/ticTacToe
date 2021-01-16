71dd2deb3a85d705804e0fbde27c1021aa4f7a6a
Keeton Martin <klmartin@haverford.edu>
2021-01-16 02:29:02 -0800

    progress made, playable game with some intelligence, but only one real path through game with 1,0 and then 1,2 (computer wins).

--- ticTacToe.py
+++ ticTacToe.py
@@ -11,13 +11,15 @@ class Board():
 		self.array = np.empty((3,3), dtype=str)
 
 	def insertAtIndex(self, i, j, letter):
+		# print(i, j)
 		self.array[i][j] = letter
 
 	def putX(self, howManyFromLeft, howManyFromTop):
 		# print(self)
+		# print("Col: ", howManyFromLeft, " Row: ", howManyFromTop)
 		self.insertAtIndex(howManyFromTop, howManyFromLeft, "X")
 		# print(self)
-		self.checkWinConditions("X")
+		# self.checkWinConditions("X")
 
 	def putO(self, howManyFromLeft, howManyFromTop):
 		# print(self)
@@ -29,7 +31,7 @@ class Board():
 		return str(self.array)
 
 	def checkWinConditions(self, letter):
-		if self.checkWonOnRow(letter): #or self.checkWonOnCol(letter) or self.checkWonOnDiag(letter):
+		if self.checkWonOnRow(letter) or self.checkWonOnCol(letter): # or self.checkWonOnDiag(letter):
 			print("Game over! ", letter, " won the game. Play again!")
 			exit()
 
@@ -46,40 +48,73 @@ class Board():
 				won = True
 		return won
 
-	def checkWonOnCol(self):
-		pass
-		
+	def checkWonOnCol(self, letter):
+		"""
+		"""
+		won = False
+		for i in range(self.size):
+			countLetters = 0
+			for j in range(self.size):
+				if self.array.T[i][j] == letter:
+					countLetters += 1
+			if countLetters == 3:
+				won = True
+		return won
+
 	def checkWonOnDiag(self):
 		pass
 
 	def chooseBestXSpot(self):
-		pass
+		temp = self.playerCouldWin("X")
+		if temp:
+			print("Winning!")
+			return temp
+		else:
+			return 2, 1
+
 
 	def playerCouldWin(self, letter):
+		rowIndex = 0
 		for row in self.array:
 			if (not otherLetter(letter) in row) and twoOccurences(letter, row):
-				ourBoard.findWinningSpot(letter, row)
+				# return True
+				targetCol = self.findWinningSpot(letter, row)
+				return rowIndex, targetCol
+			rowIndex+=1
+
+		colIndex = 0
 		for rowT in self.array.T:
-			if (not otherLetter(letter) in row) and twoOccurences(letter, row):
+			if (not otherLetter(letter) in rowT) and twoOccurences(letter, rowT):
+				# print("here and colindix: ", colIndex, " and rowT: ", rowT)
+				# return True
+				targetRow = self.findWinningSpot(letter, rowT)
+				return targetRow, colIndex
+			colIndex+=1
+
+		# Haven't checked diagonals yet
+		return None
 
 	def findWinningSpot(self, letter, row):
+		i=0
 		for possibility in row:
-			if possibility = " ":
-
+			if not possibility:		# this actually means "if we found the one"
+				return i
+			i+=1
 
 def twoOccurences(letter, row):
 	countOccurences = 0
 	for possibility in row:
 		if possibility == letter:
-			count += 1
-	if countOccurences == 2
+			countOccurences += 1
+	if countOccurences == 2:
+		return True
 
 def otherLetter(letter):
 	if letter == "X":
 		return "O"
 	elif letter == "O":
 		return "X"
-	else: error("Invalid letter!")
+	else: raise Exception("Invalid letter!")
 
 def main():
 	print("Welcome to TicTacToe AI!")
@@ -101,12 +136,12 @@ def main():
 	gameOver = False
 	while not gameOver:
 		print("My turn")
-		chosenSpot = ourBoard.chooseBestXSpot()
-		ourBoard.putX(int(tupleCoords[1]),int(tupleCoords[0]))
-
-		ourBoard.checkWinConditions("X")
+		chosenRow, chosenCol = ourBoard.chooseBestXSpot()
+		ourBoard.putX(chosenCol,chosenRow)
 
 		print(pd.DataFrame(np.matrix(ourBoard.array)))
+		ourBoard.checkWinConditions("X")
+
 		print("What's your move?")
 		coords = input("Input comma seprated coords: ")
 		tupleCoords = tuple(coords.split(","))
